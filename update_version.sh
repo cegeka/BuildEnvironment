b31dd8b73e57155da70383e087d15bc64379047b
Jan Sabbe <jan.sabbe@gmail.com>
2014-05-23 13:50:05 +0200

    Fixes in the update_version script

--- update_version.sh
+++ update_version.sh
@@ -3,4 +3,5 @@ exec > /tmp/log_version_update.log 2>&1
 
 NEW_VERSION=`date "+%Y%m%d%H%M%S"`
 echo "Updating BundleVersion to ${NEW_VERSION}";
-find $PROJECT_DIR -name '*-Info.plist' -not -name '*Tests-Info.plist' -exec /usr/libexec/PListBuddy -c "Set CFBundleVersion $NEW_VERSION" {} \;
+
+/usr/libexec/PListBuddy -c "Set CFBundleVersion $NEW_VERSION" ${SRCROOT}/${PRODUCT_NAME}/${PRODUCT_NAME}-Info.plist

010397b0de8b0fda75e632f3818c34006326be00
Jan Sabbe <jan.sabbe@gmail.com>
2014-05-26 09:17:18 +0200

    Fix for projects with spaces in them

--- update_version.sh
+++ update_version.sh
@@ -4,4 +4,4 @@
 NEW_VERSION=`date "+%Y%m%d%H%M%S"`
 echo "Updating BundleVersion to ${NEW_VERSION}";
 
-/usr/libexec/PListBuddy -c "Set CFBundleVersion $NEW_VERSION" ${SRCROOT}/${PRODUCT_NAME}/${PRODUCT_NAME}-Info.plist
+/usr/libexec/PListBuddy -c "Set CFBundleVersion $NEW_VERSION" "${SRCROOT}/${PRODUCT_NAME}/${PRODUCT_NAME}-Info.plist"
